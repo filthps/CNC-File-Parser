2593d7811298c5b7f66ff2b87e29ccd4c3cb0983
Victor-PC <filpsixi@mail.ru>
2022-06-21 20:11:59 +0300

    GUI

--- gui/application.py
+++ gui/application.py
@@ -1,26 +1,49 @@
 import sys
-from PySide2.QtWidgets import QMainWindow, QApplication, QTabWidget, QStackedWidget
-from ui import Ui_MainWindow as Ui
-from signals import MainPage
+from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtCore import QEvent, QObject
+from gui.ui import Ui_MainWindow as Ui
+from gui.signals import Navigation, Actions
+from tools import Tools
 
 
-class Tools:
-    @staticmethod
-    def __get_widget_index_by_tab_name(widget_instance: QTabWidget, tab_name: str) -> int:
-        page = widget_instance.findChild(QStackedWidget, tab_name)
-        return widget_instance.indexOf(page)
-
-
-class Main(QMainWindow, Tools, MainPage):
+class Main(QMainWindow, Tools):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
         def init_ui():
             self.ui = Ui()
             self.ui.setupUi(self)
+
+        def init_stylesheets():
+            self.ui.setStyleSheet = self.load_stylesheet("static/css/style.css")
+
+        def init_buttons():
+            self.set_icon_buttons(self.ui, "add_button", "static/img/plus.png")
+            self.set_icon_buttons(self.ui, "remove_button", "static/img/minus.png")
+            self.set_icon_buttons(self.ui, "move_right", "static/img/arrow-right.png")
+            self.set_icon_buttons(self.ui, "move_left", "static/img/arrow-left.png")
+
+        def init_navigation():
+            self.navigation = Navigation(ui=self.ui)
+
+        def init_actions():
+            self.actions = Actions(ui=self.ui)
+
+        def init_filter():
+            self.ui.root_tab_widget.installEventFilter(self)
+
         init_ui()
-        self.converter_main_page(self.ui)
-        self.set_initial_page(self.ui)
+        init_buttons()
+        init_stylesheets()
+        init_navigation()
+        init_filter()
+        init_actions()
+
+    def eventFilter(self, watched: QObject, event: QEvent):
+        def nav_to_home_page():
+            if watched.currentIndex() == 0:
+                self.navigation.nav_home_page()
+        nav_to_home_page()
 
 
 if __name__ == "__main__":
--- gui/signals.py
+++ gui/signals.py
@@ -1,16 +1,45 @@
-from typing import Callable
-from PySide2.QtWidgets import QWidget, QTabWidget, QStackedWidget
 from ui import Ui_MainWindow as Ui
 
 
-class MainPage:
+class Navigation:
+    def __init__(self, ui: Ui = None):
+        self.ui: Ui = ui
 
-    @staticmethod
-    def set_initial_page(ui: Ui):
-        ui.main_widget.setCurrentIndex(0)
+        def set_initial_page():
+            self.set_initial_page()
 
-    @staticmethod
-    def converter_main_page(ui: Ui):
-        ui.to_converter.clicked.connect(lambda: ui.main_widget.setCurrentIndex(1))
-        ui.root_tab_widget.setCurrentIndex(1)
+        def connect_signals():
+            ui.to_converter.clicked.connect(self.nav_converter_main_page)
+            ui.to_options.clicked.connect(self.nav_options_page)
+        set_initial_page()
+        connect_signals()
 
+    def set_initial_page(self):
+        self.ui.main_widget.setCurrentIndex(0)
+
+    def nav_home_page(self):
+        self.ui.main_widget.setCurrentIndex(0)
+
+    def nav_converter_main_page(self):
+        self.ui.main_widget.setCurrentIndex(1)
+        self.ui.root_tab_widget.setCurrentIndex(2)
+
+    def nav_options_page(self):
+        self.ui.main_widget.setCurrentIndex(1)
+        self.ui.root_tab_widget.setCurrentIndex(1)
+
+
+class Actions:
+    def __init__(self, ui: Ui = None):
+        self.ui: Ui = ui
+
+        def connect_signals():
+            ui.add_button_0.clicked.connect(self.add_machine)
+            ui.remove_button_0.clicked.connect(self.remove_machine)
+        connect_signals()
+
+    def add_machine(self):
+        ...
+
+    def remove_machine(self):
+        pass
