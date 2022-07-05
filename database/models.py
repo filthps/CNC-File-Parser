8ec1d0cd1aee227e4b20d8b020f8574ca29944e8
Victor-PC <filpsixi@mail.ru>
2022-07-05 23:44:28 +0300

    Применить Flask-models

--- database/models.py
+++ database/models.py
@@ -1,57 +1,58 @@
 from uuid import uuid4
-from sqlalchemy import MetaData, Integer, String, Column, ForeignKey, CheckConstraint, create_engine, Table, select, inspect, PrimaryKeyConstraint
-from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask
+from sqlalchemy import Integer, String, Column, CheckConstraint
+from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + '../database.db'
+db = SQLAlchemy(app)
 
 
-Data = MetaData()
-Base = declarative_base()
+AssociationTable = db.Table('association',
+                            db.Column('machine_id', db.Integer, db.ForeignKey('machine.machine_id')),
+                            db.Column('operation_id', db.String, db.ForeignKey('operation.opid')),
+                            )
 
 
 def get_uuid():
     return str(uuid4())
 
 
-association_table = Table("association", Data,
-                          Column('machine_id', ForeignKey('machine.machine_id')),
-                          Column('operation_id', ForeignKey('operation.opid')),
-                          )
+class Machine(db.Model):
+    __tablename__ = "machine"
+    machine_id = Column(Integer, primary_key=True, autoincrement=True)
+    machine_name = Column(String(100), nullable=False, unique=True)
+    x_over = Column(Integer, nullable=True)
+    y_over = Column(Integer, nullable=True)
+    z_over = Column(Integer, nullable=True)
+    x_fspeed = Column(Integer, nullable=True)
+    y_fspeed = Column(Integer, nullable=True)
+    z_fspeed = Column(Integer, nullable=True)
+    spindele_speed = Column(Integer, nullable=True)
+    input_catalog = Column(String, nullable=False)
+    output_catalog = Column(String, nullable=False)
+    operations = relationship('Operation', secondary=AssociationTable)
+    __table__args = (
+        CheckConstraint('input_catalog!=""'),
+        CheckConstraint('output_catalog!=""'),
+    )
 
 
-Machine = Table("machine", Data,
-                Column('machine_id', Integer(), primary_key=True, autoincrement=True),
-                Column('machine_name', String(100), nullable=False, unique=True),
-                Column('x_over', Integer(), nullable=True),
-                Column('y_over', Integer(), nullable=True),
-                Column('z_over', Integer(), nullable=True),
-                Column('x_fspeed', Integer(), nullable=True),
-                Column('y_fspeed', Integer(), nullable=True),
-                Column('z_fspeed', Integer(), nullable=True),
-                Column('spindele_speed', Integer(), nullable=True),
-                Column('input_catalog', String(), nullable=False),
-                Column('output_catalog', String(), nullable=False),
-                Column("operation_id", ForeignKey('association.operation_id')),
-                CheckConstraint('input_catalog!=""'),
-                CheckConstraint('output_catalog!=""'),
-                )
-
-
-Operation = Table('operation', Data,
-                  Column("machine_id", ForeignKey('association.machine_id')),
-                  Column('opid', String(), primary_key=True, default=get_uuid),
-                  Column('operation_type', String(), nullable=False),
-                  Column('operation_name', String(), nullable=False),
-                  Column('target', String(), nullable=False),
-                  Column('after_or_before', String(), nullable=False),
-                  Column('item', String(), nullable=False),
-                  CheckConstraint('operation_type IN("a", "r", "d", "c")', name='type_choice'),
-                  CheckConstraint('after_or_before IN("a", "b")', name='a_or_b'),
-                  )
+class Operation(db.Model):
+    __tablename__ = 'operation'
+    machines = relationship('Machine', secondary=AssociationTable)
+    opid = Column(String, primary_key=True, default=get_uuid)
+    operation_type = Column(String, nullable=False)
+    operation_name = Column(String, nullable=False)
+    target = Column(String, nullable=False)
+    after_or_before = Column(String, nullable=False)
+    item = Column(String, nullable=False)
+    __table__args = (
+        CheckConstraint('operation_type IN("a", "r", "d", "c")', name='type_choice'),
+        CheckConstraint('after_or_before IN("a", "b")', name='a_or_b'),
+    )
 
 
 if __name__ == "__main__":
-    engine = create_engine("sqlite:///../database.db")
-    engine.connect()
-    Data.create_all(engine)
-    #Data.drop_all(engine)
-    s = Machine.select()
-    print(engine.execute(s).fetchone())
+    print(Machine.query.all())
