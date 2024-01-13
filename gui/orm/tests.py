b3f9f69f36beaa9e0533840bf56180673100035a
Виктор Литовченко <filpsixi@mail.ru>
2024-01-14 01:42:58 +0300

    tests.py

--- gui/orm/tests.py
+++ gui/orm/tests.py
@@ -2,16 +2,49 @@ import unittest
 import time
 from sqlalchemy import func, select
 from database.models import Machine, Cnc, db as sqlalchemy_instance
+from database.procedures import init_all_triggers
 from orm import *
 from exceptions import *
 
 
+def is_database_empty(session, empty=True, tables=15, procedures=52, test_db_name="testdb"):
+    table_counter = session.execute('SELECT COUNT(table_name) '
+                                    'FROM information_schema."tables" '
+                                    'WHERE table_type=\'BASE TABLE\' AND table_schema=\'public\';').scalar()
+    procedures_counter = session.execute(f'SELECT COUNT(*) '
+                                         f'FROM information_schema."triggers" '
+                                         f'WHERE trigger_schema=\'public\' AND '
+                                         f'trigger_catalog=\'{test_db_name}\' AND '
+                                         f'event_object_catalog=\'{test_db_name}\';').scalar()
+    if empty:
+        if table_counter or procedures_counter:
+            time.sleep(1)
+            return is_database_empty(session, empty=empty, tables=tables, procedures=procedures,
+                                     test_db_name=test_db_name)
+        return True
+    if table_counter < tables or procedures_counter < procedures:
+        time.sleep(1)
+        return is_database_empty(session, empty=empty, tables=tables, procedures=procedures,
+                                 test_db_name=test_db_name)
+    return True
+
+
 def db_reinit(m):
-    def wrap(*args, **kwargs):
+    def wrap(self: "TestORMHelper", *args, **kwargs):
         sqlalchemy_instance.drop_all()
-        sqlalchemy_instance.create_all()
-        return m(*args, **kwargs)
+        if is_database_empty(self.orm_manager.database):
+            sqlalchemy_instance.create_all()
+            init_all_triggers()
+            if is_database_empty(self.orm_manager.database, empty=False):
+                return m(self, *args, **kwargs)
     return wrap
+
+
+def drop_cache(callable_):
+    def w(self: "TestORMHelper", *args, **kwargs):
+        self.orm_manager.drop_cache()
+        return callable_(self, *args, **kwargs)
+    return w
 
 
 class TestORMHelper(unittest.TestCase):
@@ -78,3 +111,17 @@ class TestORMHelper(unittest.TestCase):
                                                            "FROM machine "
                                                            "WHERE machine.cncid=(SELECT cncid FROM cnc WHERE name = 'testcnc')"
                                                            ).scalar(), 1)
+
+    def test_items_property(self):
+        self.assertTrue(type(self.orm_manager.items) is ORMItemQueue)
+
+    def test_init_timer(self):
+        t = self.orm_manager.init_timer()
+        self.assertIsInstance(t, threading.Timer)
+        self.assertEqual(t.getName(), "ORMHelper(database push queue)")
+
+    @drop_cache
+    @db_reinit
+    def test_set_item(self):
+        self.orm_manager.set_item(_insert=True, _model=Cnc, name="Fid")
+        self.assertTrue(self.orm_manager.items.__len__(), 1)
